e85c3b67128b688ef07d8a7b81a63c87d5ae7aaf
Nikita <nikitapolin47@gmail.com>
2023-08-09 15:28:40 +1000

    Initial commit

---
+++ src/main/java/com/nikik0/finApi/FinApiApplication.java
@@ -0,0 +1,13 @@
+package com.nikik0.finApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikik0/finApi/FinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikik0.finApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
