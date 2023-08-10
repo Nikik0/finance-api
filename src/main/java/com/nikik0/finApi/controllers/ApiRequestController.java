c3d6bacc5c2ed5bb5107061ef85fe3c95d8e3a8c
Nikita <nikitapolin47@gmail.com>
2023-08-10 15:24:48 +1000

    Parametrized call to external api for proxy

--- src/main/java/com/nikik0/finApi/controllers/ApiRequestController.java
+++ src/main/java/com/nikik0/finApi/controllers/ApiRequestController.java
@@ -3,17 +3,19 @@ package com.nikik0.finApi.controllers;
 import com.nikik0.finApi.apiProxy.ExternalApiProxy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("fin")
 @RequiredArgsConstructor
 @Slf4j
 public class ApiRequestController {
-
     private final ExternalApiProxy externalApiProxy;
 
     @RequestMapping("/test")
@@ -21,8 +23,21 @@ public class ApiRequestController {
         externalApiProxy.call().flatMap(response ->
                 {
                     log.info("received response " + response);
+                    log.info("received class " + response.getClass());
                     return Mono.just(response);
                 }
         ).subscribe();
     }
+    @RequestMapping("/test2")
+    public void testCall2(){
+        externalApiProxy.performCallToExternalApi("&symbols=aapl", Object.class, HttpMethod.GET).flatMap(
+                response ->{
+                    log.info("received response " + response);
+                    return Mono.just(response);
+                }
+        ).subscribe();
+    }
+
+
+
 }
