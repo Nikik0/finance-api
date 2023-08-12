5e08500215e7630ded56c4b5d819238b89080fba
Nikita <nikitapolin47@gmail.com>
2023-08-12 13:17:59 +1000

    rewind of spring batch usage coz of problems with r2dbc

--- src/main/java/com/nikik0/finApi/FinApiApplication.java
+++ src/main/java/com/nikik0/finApi/FinApiApplication.java
@@ -3,7 +3,7 @@ package com.nikik0.finApi;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableBatchProcessing
+
 @SpringBootApplication
 public class FinApiApplication {
 
